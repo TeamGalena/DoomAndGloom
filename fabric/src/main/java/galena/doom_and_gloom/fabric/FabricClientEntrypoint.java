cffe0b887bdcccb29499479e706a9d16af00b194
Niklas Widmann <nik@somethingcatchy.net>
2025-11-06 13:23:32 +0100

    actually use fabric mixins & register particles there

--- fabric/src/main/java/galena/doom_and_gloom/fabric/FabricClientEntrypoint.java
+++ fabric/src/main/java/galena/doom_and_gloom/fabric/FabricClientEntrypoint.java
@@ -4,6 +4,10 @@ import galena.doom_and_gloom.client.DoomAndGloomClient;
 import galena.doom_and_gloom.client.fog.FogRendering;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
 
 public class FabricClientEntrypoint implements ClientModInitializer {
 
@@ -11,6 +15,14 @@ public class FabricClientEntrypoint implements ClientModInitializer {
     public void onInitializeClient() {
         DoomAndGloomClient.init();
         DoomAndGloomClient.setup();
+
+        var particleRegistry = ParticleFactoryRegistry.getInstance();
+        DoomAndGloomClient.registerParticleFactories(new ClientHelper.ParticleEvent() {
+            @Override
+            public <P extends ParticleType<T>, T extends ParticleOptions> void register(P type, ClientHelper.ParticleFactory<T> factory) {
+                particleRegistry.register(type, factory::create);
+            }
+        });
 
         ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
             FogRendering.clientTick();
